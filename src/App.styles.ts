bd1ab8175c77ba160f5a9939342a412a6f07b4b4
AnaMickaelly <anamickaellyd2@gmail.com>
2022-02-09 17:20:26 -0300

    first publications of the game

---
+++ src/App.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #24282f;
+  min-height: 100vh;
+  color: #fff;
+`;
+export const Map = styled.div`
+  width: 651px;
+  height: 654px;
+  background-image: url('./src/assets/map.png');
+  background-position: left top;
+  background-size: 100%;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as C from './App.styles';
+import { Character } from './components/Character';
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Map>
+        <Character x={30} y={30} />
+      </C.Map>
+    </C.Container>
+  );
+};
+export default App;
